73b74883ed7a0a5fcb2343e5db36c6e602977501
CEF440-Group-27 <165272407+CEF440-Group-27@users.noreply.github.com>
2024-07-03 19:58:25 +0100

    Add files via upload

---
+++ Source code/frontend/SignInPage.tsx
@@ -0,0 +1,75 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable prettier/prettier */
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import styles from '../CSS/Styles';
+
+import {Button, Avatar} from 'react-native-paper';
+import {customTheme} from '../CSS/colors';
+
+import {AuthenticationService} from '../Services/AuthenticationService';
+
+
+function SignInPage({navigation}) {
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+
+  const authService = new AuthenticationService({navigation});
+
+  return (
+    <SafeAreaView style={styles.container}>
+       <View style={{flexDirection: 'row', marginLeft: 20,marginBottom: 10}}>
+            <Avatar.Icon size={70} icon='car'/>
+            <Text style={{fontSize: 40, marginTop: 10,  fontWeight: 'bold'}}>DriveSafe</Text>
+            </View>
+        <View>
+        <Text>Email</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setEmail}
+        value={email}
+        placeholder="@email"
+      />
+        </View>
+      <View>
+      <Text>Password</Text>
+      <TextInput
+        secureTextEntry={true}
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+      />
+      </View>
+      <View>
+      <Button
+          mode="contained"
+          theme={customTheme}
+          onPress={() => {
+            authService.EmailSignIn(email, password);
+          }}
+          >
+          Sign In
+        </Button>
+      </View>
+      <View>
+        <TouchableOpacity onPress={() => navigation.navigate('Reset Password')}>
+        <Text style = {{color: 'blue', marginTop: 20, marginBottom: 20, fontSize: 25}}> Forgot your password? </Text>
+        </TouchableOpacity>
+        <Text style = {{marginTop: 20, marginBottom: 20, fontSize: 25}}> Do you have an account? </Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Sign Up')}>
+        <Text style = {{color: 'blue', marginTop: 20, marginBottom: 20, fontSize: 25}}> Sign Up </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default SignInPage;
+
